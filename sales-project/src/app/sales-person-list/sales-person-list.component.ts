064126d241e82579189f4cb932a71bc791aa9e5c
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-02-16 16:56:46 +0530

    Implementing Table in Angular using new components and classes

---
+++ sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from "@angular/core";
+import { SalesPerson } from "./sales-person";
+
+@Component({
+  selector: "app-sales-person-list",
+  templateUrl: "./sales-person-list.component.html",
+  styleUrls: ["./sales-person-list.component.css"]
+})
+export class SalesPersonListComponent implements OnInit {
+  // create an array of objects
+
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Anoop", "Gupta", "aaa@getMaxListeners.com", 500),
+    new SalesPerson("Sarthak", "Gupta", "sarthakgupta98@gmail.com", 1000),
+    new SalesPerson("Gupta", "Sarthak", "aaabbb@gmail.com", 10000)
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
